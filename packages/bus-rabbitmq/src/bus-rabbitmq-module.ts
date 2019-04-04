ae6f629c8618b5c500b0e09277f7222c397b0938
Andrew den Hertog <andrew.denhertog@gmail.com>
2019-04-04 16:52:41 +1000

    provide module rebinds for rabbit

--- packages/bus-rabbitmq/src/bus-rabbitmq-module.ts
+++ packages/bus-rabbitmq/src/bus-rabbitmq-module.ts
@@ -4,14 +4,16 @@ import { connect } from 'amqplib'
 import { RabbitMqTransport } from './rabbitmq-transport'
 import { bindLogger } from '@node-ts/logger-core'
 import { RabbitMqTransportConfiguration } from './rabbitmq-transport-configuration'
+import { BUS_SYMBOLS, Transport } from '@node-ts/bus-core'
 
 export class BusRabbitMqModule extends ContainerModule {
   constructor () {
-    super (async bind => {
+    super (async (bind, _, __, rebind) => {
       bind(BUS_RABBITMQ_INTERNAL_SYMBOLS.RabbitMqTransport)
         .to(RabbitMqTransport)
         .inSingletonScope()
       bindLogger(bind, RabbitMqTransport)
+      rebind<Transport<{}>>(BUS_SYMBOLS.Transport).to(RabbitMqTransport)
 
       bind(BUS_RABBITMQ_INTERNAL_SYMBOLS.AmqpFactory)
         .toFactory(c => async () => {
@@ -19,6 +21,7 @@ export class BusRabbitMqModule extends ContainerModule {
             .get<RabbitMqTransportConfiguration>(BUS_RABBITMQ_SYMBOLS.TransportConfiguration)
           return connect(configuration.connectionString)
         })
+
     })
   }
 }
