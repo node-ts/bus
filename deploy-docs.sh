2601dc88960b3d9460ac4b53d2779d93808f393c
Andrew den Hertog <andrew.denhertog@gmail.com>
2019-04-25 10:24:03 +1000

    add local deploy docs

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -9,6 +9,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:node-ts/bus.git master:gh-pages
+git push -f https://github.com/node-ts/bus.git master:gh-pages
 
 cd -
+rm -rf docs
