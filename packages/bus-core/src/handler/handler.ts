fa32490ea767f88d520b4f2b37d5350559f619d8
Andrew den Hertog <andrew.denhertog@gmail.com>
2019-10-10 07:22:01 +1000

    always return handler promise to fix TS typings

--- packages/bus-core/src/handler/handler.ts
+++ packages/bus-core/src/handler/handler.ts
@@ -11,7 +11,8 @@ export interface HandlerPrototype {
  * An interface used by `HandlesMessages` used to dispatch messages to
  * @param message A message that has been received from the bus and passed to the handler for processing
  * @param options (optional) Additional message options and metadata that were sent along with the message
+ * @returns An awaitable promise that resolves when the handler operation has completed
  */
 export interface Handler<TMessage extends Message> {
-  handle (message: TMessage, messageOptions?: MessageAttributes): Promise<void> | void
+  handle (message: TMessage, messageOptions?: MessageAttributes): Promise<void>
 }
