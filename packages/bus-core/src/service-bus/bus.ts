737ab0d88e1c80099561c25dde4d1a4f3aad00e9
Andrew den Hertog <andrew.denhertog@gmail.com>
2020-06-10 11:53:22 +1000

    expose bus state through interface

--- packages/bus-core/src/service-bus/bus.ts
+++ packages/bus-core/src/service-bus/bus.ts
@@ -11,9 +11,22 @@ export type HookAction = 'send' | 'publish'
 export type HookCallback = (message: Message, messageAttributes?: MessageAttributes) => Promise<void> | void
 
 export interface Bus {
+  /**
+   * Fetches the state of the message read and processing loop
+   */
+  state: BusState
+
   publish<EventType extends Event> (event: EventType, messageOptions?: MessageAttributes): Promise<void>
   send<CommandType extends Command> (command: CommandType, messageOptions?: MessageAttributes): Promise<void>
+
+  /**
+   * For applications that handle messages, start reading messages off the underlying queue and process them.
+   */
   start (): Promise<void>
+
+  /**
+   * For  applications that handle messages, stop reading messages from the underlying queue.
+   */
   stop (): Promise<void>
 
   /**
