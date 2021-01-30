aef18901d8e6e79c6e890d17b330476bcbd66441
Andrew den Hertog <andrew.denhertog@gmail.com>
2021-01-30 21:51:05 +1000

    check for whole transport message

--- packages/bus-core/src/service-bus/bus-hooks.ts
+++ packages/bus-core/src/service-bus/bus-hooks.ts
@@ -5,22 +5,26 @@ import { HookCallback, HookAction, ErrorHookCallback, StandardHookCallback } fro
  * A singleton repository for all hook events registered with the Bus. This persists across scope boundaries
  * so that Bus instances that are scoped to a message handling context still have access to the global set
  * of registered hooks
+ *
+ * @template TransportMessageType - The raw message type returned from the transport that will be passed to the hooks
+ * @example BusHooks<InMemoryMessage>
+ * @example BusHooks<SQS.Message>
  */
 @injectable()
-export class BusHooks {
-  private messageHooks: { [key: string]: HookCallback[] } = {
+export class BusHooks<TransportMessageType = unknown> {
+  private messageHooks: { [key: string]: HookCallback<TransportMessageType>[] } = {
     send: [],
     publish: [],
     error: []
   }
 
-  on (action: Extract<HookAction, 'error'>, callback: ErrorHookCallback): void
+  on (action: Extract<HookAction, 'error'>, callback: ErrorHookCallback<TransportMessageType>): void
   on (action: Exclude<HookAction, 'error'>, callback: StandardHookCallback): void
-  on (action: HookAction, callback: HookCallback): void {
+  on (action: HookAction, callback: HookCallback<TransportMessageType>): void {
     this.messageHooks[action].push(callback)
   }
 
-  off (action: HookAction, callback: HookCallback): void {
+  off (action: HookAction, callback: HookCallback<TransportMessageType>): void {
     const index = this.messageHooks[action].indexOf(callback)
     if (index >= 0) {
       this.messageHooks[action].splice(index, 1)
@@ -35,7 +39,7 @@ export class BusHooks {
     return this.messageHooks.publish as StandardHookCallback[]
   }
 
-  get error (): ErrorHookCallback[] {
-    return this.messageHooks.error as ErrorHookCallback[]
+  get error (): ErrorHookCallback<TransportMessageType>[] {
+    return this.messageHooks.error as ErrorHookCallback<TransportMessageType>[]
   }
 }
