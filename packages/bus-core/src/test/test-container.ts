cffaa3e5cfd1eae3887fc7691e9f9d6cbd8ca6d0
Andrew den Hertog <andrew.denhertog@gmail.com>
2021-08-03 21:02:29 +1000

    upgrade packages

--- packages/bus-core/src/test/test-container.ts
+++ packages/bus-core/src/test/test-container.ts
@@ -4,7 +4,6 @@ import { Container } from 'inversify'
 import { LoggerModule, LOGGER_SYMBOLS, LoggerFactory, Logger } from '@node-ts/logger-core'
 import { BusModule } from '../bus-module'
 import { Mock, It } from 'typemoq'
-import { assertUnreachable } from '../util'
 
 export class TestContainer extends Container {
 
@@ -17,33 +16,12 @@ export class TestContainer extends Container {
   /**
    * Swallows logs during integration test runs. Useful in not cluttering up
    * the test output.
-   *
-   * @param errorLevel The error to dispay logs for. @default 'warn'
    */
-  silenceLogs (errorLevel: keyof Logger = 'warn'): this {
+  silenceLogs (): this {
     const factory = this.get<LoggerFactory>(LOGGER_SYMBOLS.LoggerFactory)
     const logger = factory.build('test-logger', this)
-
     const mockLogger = Mock.ofInstance(logger)
-    logsToOutput(errorLevel).forEach(level => {
-      mockLogger
-        .setup(l => l[level](It.isAnyString(), It.isAny()))
-        .callBase()
-    })
-
     this.rebind(LOGGER_SYMBOLS.Logger).toConstantValue(mockLogger.object)
     return this
   }
-}
-
-function logsToOutput (errorLevel: keyof Logger): (keyof Logger)[] {
-  const levels: (keyof Logger)[] = [
-    'debug',
-    'trace',
-    'info',
-    'warn',
-    'error',
-    'fatal'
-  ]
-  return levels.slice(levels.indexOf(errorLevel), levels.length)
 }
