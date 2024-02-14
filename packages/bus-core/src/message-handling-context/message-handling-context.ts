8a98004ea292950dcceea400cadac7bead88c5c1
adenhertog <andrew.denhertog@gmail.com>
2024-02-14 11:19:40 +1000

    Cutover message handling context to ALS Context (#220)
    * cutover to als context
    
    * update tests

--- packages/bus-core/src/message-handling-context/message-handling-context.ts
+++ packages/bus-core/src/message-handling-context/message-handling-context.ts
@@ -1,48 +1,27 @@
-import {
-  createNamespace,
-  destroyNamespace,
-  getNamespace,
-  Namespace
-} from 'cls-hooked'
 import { TransportMessage } from '../transport'
+import ALS from 'alscontext'
 
-const NAMESPACE = 'message-handling-context'
-let namespace: Namespace
-
-/**
- * This is an internal coordinator that tracks the execution context for
- * a message handling operation across multiple nested promises/callbacks as a way
- * of providing a type of thread local storage (TLS).
- *
- * It's primarily used to allow Bus.send/Bus.publish to propagate sticky attributes
- * and correlation ids in parallel handling contexts.
- */
-export const messageHandlingContext = {
+class MessageHandlingContext extends ALS {
   /**
-   * Executes a function within a context of cls-hooked and returns the result
+   * Fetch the message context for the current async stack
    */
-  runAndReturn: <T>(fn: (...args: any[]) => T) => namespace.runAndReturn(fn),
+  get(): TransportMessage<unknown> {
+    return super.get('message')
+  }
 
   /**
-   * Sets a new handling context for the current execution async id. Child asyncs should
-   * only call this if they want to create a new context with themselves at the root.
+   * Set the message context for the current async stack
    */
-  set: (message: TransportMessage<unknown>) =>
-    namespace?.set('message', message),
+  set(message: TransportMessage<unknown>) {
+    return super.set('message', message)
+  }
+
   /**
-   * Fetches the message handling context of the active async stack
+   * Start and run a new async context
    */
-  get: () => namespace?.get('message') as TransportMessage<unknown>,
-  /**
-   * Hooks into the async_hooks module to track the current execution context. Must be called before other operations.
-   */
-  enable: () => (namespace = createNamespace(NAMESPACE)),
-  /**
-   * Stops tracking the current execution context.
-   */
-  disable: () => {
-    if (getNamespace(NAMESPACE)) {
-      destroyNamespace(NAMESPACE)
-    }
+  run<T>(context: TransportMessage<unknown>, fn: () => T | Promise<T>) {
+    return super.run({ message: context }, fn)
   }
 }
+
+export const messageHandlingContext = new MessageHandlingContext()
