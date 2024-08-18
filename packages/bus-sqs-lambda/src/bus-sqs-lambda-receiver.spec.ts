c6faf39f2b899821ace65fa9a8ead6c4cfa54807
Andrew den Hertog <andrew.denhertog@gmail.com>
2024-08-19 08:53:17 +1000

    fix message attrib parsing

--- packages/bus-sqs-lambda/src/bus-sqs-lambda-receiver.spec.ts
+++ packages/bus-sqs-lambda/src/bus-sqs-lambda-receiver.spec.ts
@@ -1,29 +1,36 @@
-import { JsonSerializer, MessageSerializer } from '@node-ts/bus-core'
+import {
+  JsonSerializer,
+  MessageSerializer,
+  TransportMessage
+} from '@node-ts/bus-core'
 import { BusSqsLambdaReceiver } from './bus-sqs-lambda-receiver'
-import { Message } from '@node-ts/bus-messages'
+import { Message, MessageAttributes } from '@node-ts/bus-messages'
+import { SQSRecord } from 'aws-lambda'
 
-const payload = {
+const attributePayload = {
   Records: [
     {
-      messageId: '059f36b4-87a3-44ab-83d2-661975830a7d',
-      receiptHandle: 'AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...',
-      body: '{"foo":"bar"}',
-      attributes: {
-        ApproximateReceiveCount: '1',
-        SentTimestamp: '1545082649183',
-        SenderId: 'AIDAIENQZJOLO23YVJ4VO',
-        ApproximateFirstReceiveTimestamp: '1545082649185'
-      },
-      messageAttributes: {},
-      md5OfBody: 'e4e68fb7bd0e697a0ae8f1bb342846b3',
-      eventSource: 'aws:sqs',
-      eventSourceARN: 'arn:aws:sqs:us-east-2:123456789012:my-queue',
-      awsRegion: 'us-east-2'
-    },
-    {
-      messageId: '2e1424d4-f796-459a-8184-9c92662be6da',
-      receiptHandle: 'AQEBzWwaftRI0KuVm4tP+/7q1rGgNqicHq...',
-      body: '{"golf":"hotel"}',
+      messageId: '4b9a650d-00fa-4f86-a4f2-d57661155b94',
+      receiptHandle:
+        'AQEByTon76mjc9w6jPFA+C1P3bOC5B6ayx1ap0VVvTbCy9w0Patfwnc0y8qHPFFPCxkiszTHkRhHBiLgrkeQ32rRCRz0ZJr1mL7ekYnKS/GndtwKAw/hBZW2vKFiHc9bMxG6DmlBkeNc9QmIGdJRplYhIEmDUd3ckuU7FegGvODiwfTpjxe5bz2Q7T7aS85iKJ6ZTtAIZiHhLKOQYwxkAqXiCB4nOsFYRul+rSLfw3oDhUgG9mqEJZnPqdkifUFFEwaGzx6q9HIOLB/J6S8ZDxoLTR2yOJESnBywZJzzHUuADBOn1IBImOxV3vfpHSAuxgibZSuz1vlFV50x3l6i14t/RQZsujnLn/w+1w0XpW2//0AmGrZvib+YfHDZQ2UjgzDbVPED+zJtpH1wYw5HzP/4jg==',
+      body:
+        '{\n' +
+        '  "Type" : "Notification",\n' +
+        '  "MessageId" : "31fe335f-39c7-5124-bcf7-6c634835eb49",\n' +
+        '  "TopicArn" : "arn:aws:sns:us-west-2:339712791595:zerodual-staging-audit-create-audit-log",\n' +
+        '  "Subject" : "audit/create-audit-log",\n' +
+        '  "Message" : "{\\"id\\":\\"8c6bc223-5437-4de8-b999-8a742d65784b\\",\\"userId\\":\\"e04010bd-e2fa-492f-b680-54e586680da2\\",\\"ip\\":\\"dc71:b6ca:6974:bff9:c17c:2fe3:5de1:deb3\\",\\"operations\\":[],\\"metadata\\":{},\\"$name\\":\\"audit/create-audit-log\\",\\"$version\\":0,\\"createdAt\\":\\"2024-08-18T22:03:27.780Z\\"}",\n' +
+        '  "Timestamp" : "2024-08-18T22:03:30.355Z",\n' +
+        '  "SignatureVersion" : "1",\n' +
+        '  "Signature" : "...",\n' +
+        '  "SigningCertURL" : "https://sns.us-west-2.amazonaws.com/SimpleNotificationService-...",\n' +
+        '  "UnsubscribeURL" : "https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=...",\n' +
+        '  "MessageAttributes" : {\n' +
+        '    "correlationId" : {"Type":"String","Value":"e3808fce-9b66-4596-b528-479b72e598fe"},\n' +
+        '    "stickyAttributes.x-sticky-attribute" : {"Type":"String","Value":"baz"},\n' +
+        '    "attributes.x-foo-attribute" : {"Type":"String","Value":"bar"}\n' +
+        '  }\n' +
+        '}',
       attributes: {
         ApproximateReceiveCount: '1',
         SentTimestamp: '1545082650636',
@@ -31,10 +38,10 @@ const payload = {
         ApproximateFirstReceiveTimestamp: '1545082650649'
       },
       messageAttributes: {},
-      md5OfBody: 'e4e68fb7bd0e697a0ae8f1bb342846b3',
+      md5OfBody: 'f54bafbb90f2526eeecbed33fe71d668',
       eventSource: 'aws:sqs',
-      eventSourceARN: 'arn:aws:sqs:us-east-2:123456789012:my-queue',
-      awsRegion: 'us-east-2'
+      eventSourceARN: 'arn:aws:sqs:us-west-2:339712791595:temp-sqs',
+      awsRegion: 'us-west-2'
     }
   ]
 }
@@ -54,13 +61,43 @@ describe('BusSqsLambdaReceiver', () => {
   const receiver = new BusSqsLambdaReceiver()
   const serializer = new TestMessageSerializer(new JsonSerializer(), {} as any)
 
-  describe('when lambda receives a batch of messages from SQS', () => {
-    it('should parse them into TransportMessages', async () => {
-      const messages = await receiver.receive(payload, serializer)
-      expect(messages).toHaveLength(2)
+  describe('when lambda receives a message with attributes', () => {
+    let attributes: MessageAttributes
+    let messages: TransportMessage<SQSRecord>[]
 
-      expect(messages[0].domainMessage).toMatchObject({ foo: 'bar' })
-      expect(messages[1].domainMessage).toMatchObject({ golf: 'hotel' })
+    beforeAll(async () => {
+      messages = await receiver.receive(attributePayload, serializer)
+      attributes = messages[0].attributes
+    })
+
+    it('should parse out the body', () => {
+      expect(messages).toHaveLength(1)
+      expect(messages[0].domainMessage).toMatchObject({
+        id: '8c6bc223-5437-4de8-b999-8a742d65784b',
+        userId: 'e04010bd-e2fa-492f-b680-54e586680da2',
+        ip: 'dc71:b6ca:6974:bff9:c17c:2fe3:5de1:deb3',
+        operations: [],
+        metadata: {},
+        $name: 'audit/create-audit-log',
+        $version: 0,
+        createdAt: '2024-08-18T22:03:27.780Z'
+      })
+    })
+
+    it('should parse out the correlationId', () => {
+      expect(attributes.correlationId).toEqual(
+        'e3808fce-9b66-4596-b528-479b72e598fe'
+      )
+    })
+
+    it('should parse out attributes', () => {
+      expect(attributes.attributes).toMatchObject({ 'x-foo-attribute': 'bar' })
+    })
+
+    it('should parse out sticky attributes', () => {
+      expect(attributes.stickyAttributes).toMatchObject({
+        'x-sticky-attribute': 'baz'
+      })
     })
   })
 })
--- packages/bus-sqs-lambda/src/bus-sqs-lambda-receiver.ts
+++ packages/bus-sqs-lambda/src/bus-sqs-lambda-receiver.ts
@@ -3,8 +3,8 @@ import {
   Receiver,
   TransportMessage
 } from '@node-ts/bus-core'
+import { fromMessageAttributeMap } from '@node-ts/bus-sqs'
 import type { SQSEvent, SQSRecord } from 'aws-lambda'
-import { fromMessageAttributeMap } from './from-message-attribute-map'
 
 /**
  * Receives messages from an SQS event that's triggered a lambda function, and converts them into a TransportMessage
@@ -17,11 +17,17 @@ export class BusSqsLambdaReceiver
     receivedMessage: SQSEvent,
     messageSerializer: MessageSerializer
   ): Promise<TransportMessage<SQSRecord>[]> {
-    return receivedMessage.Records.map(record => ({
-      id: record.messageId,
-      domainMessage: messageSerializer.deserialize(record.body),
-      raw: record,
-      attributes: fromMessageAttributeMap(record.messageAttributes)
-    }))
+    return receivedMessage.Records.map(record => {
+      const body = JSON.parse(record.body)
+      const domainMessage = messageSerializer.deserialize(body.Message)
+      const attributes = fromMessageAttributeMap(body.MessageAttributes)
+
+      return {
+        id: record.messageId,
+        domainMessage,
+        raw: record,
+        attributes
+      }
+    })
   }
 }
